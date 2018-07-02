40bc272d9a62b0a410599025fcff38ddac2b6274
Yuan Fu <casouri@gmail.com>
2018-07-02 15:19:02 +0800

    Update

--- camelsnake-mode.el
+++ camelsnake-mode.el
@@ -17,23 +17,26 @@ char-before-list sets `camelsnake-char-before-list',
 delimiter sets `camelsnake-delimiter',
 judge sets `camelsnake-disable-judge'.")
 
-(defvar camelsnake-char-list '(?A ?B ?C ?D ?E ?F ?G ?H ?I ?J ?K ?L ?M ?N ?O ?P ?Q ?R ?S ?T ?U ?V ?W ?X ?Y ?Z)
+(defvar-local camelsnake-char-list '(?A ?B ?C ?D ?E ?F ?G ?H ?I ?J ?K ?L ?M ?N ?O ?P ?Q ?R ?S ?T ?U ?V ?W ?X ?Y ?Z)
   "Characters that camelsnake will modify.")
 
-(defvar camelsnake-char-before-list '(?a ?b ?c ?d ?e ?f ?g ?h ?i ?j ?k ?l ?m ?n ?o ?p ?q ?r ?s ?t ?u ?v ?w ?x ?y ?z)
+(defvar-local camelsnake-char-before-list '(?a ?b ?c ?d ?e ?f ?g ?h ?i ?j ?k ?l ?m ?n ?o ?p ?q ?r ?s ?t ?u ?v ?w ?x ?y ?z)
   "Characters before the just inserted character that will make camelsnake modify input.")
 
-(defvar camelsnake-delimiter ?-
+(defvar-local camelsnake-delimiter ?-
   "The default delimiter.")
 
-(defvar camelsnake-disable-judge nil
+(defvar-local camelsnake-disable-judge nil
   "The user defined function that decides if camelsnake should modify character.
 This function is called when user just typed the critical character, e.g.
-\"camcelC|\" \(\"|\" is the point\).
+\"camelC|\" \(\"|\" is the point\).
 
 This function takes no argument, return t to allow modification, return nil otherwise.
 
 Mind that this function is called in `post-self-insert-hook', so don't put heavy stuff in it.")
+
+(defvar-local camelsnake--enable-locally nil
+  "Don't set this manually, configured by `camelsnake-configure-on-major-mode'.")
 
 ;;
 ;; Entry
@@ -59,7 +62,8 @@ Mind that this function is called in `post-self-insert-hook', so don't put heavy
         (before-inserted (char-before (1- (point)))))
     ;; if the char just inserted is a Captical _and_
     ;; the one before it is not, insert the delimiter.
-    (when (and (member just-inserted camelsnake-char-list)
+    (when (and camelsnake--enable-locally
+               (member just-inserted camelsnake-char-list)
                (member before-inserted camelsnake-char-before-list)
                (if camelsnake-disable-judge
                    (funcall camelsnake-disable-judge)
@@ -79,22 +83,31 @@ Mind that this function is called in `post-self-insert-hook', so don't put heavy
          (delimiter (alist-get 'delimiter spec-alist))
          (on (alist-get 'on spec-alist))
          (judge (alist-get 'judge spec-alist)))
-    (unless on (remove-hook 'post-self-insert-hook #'camelsnake-modify-insert t))
-    (when char-list (setq-local camelsnake-char-list char-list))
-    (when char-before-list (setq-local camelsnake-char-before-list char-before-list))
-    (when delimiter (setq-local camelsnake-delimiter delimiter))
-    (when judge (setq-local camelsnake-disable-judge judge))))
+    (setq camelsnake--enable-locally on)
+    (when char-list (setq camelsnake-char-list char-list))
+    (when char-before-list (setq camelsnake-char-before-list char-before-list))
+    (when delimiter (setq camelsnake-delimiter delimiter))
+    (when judge (setq camelsnake-disable-judge judge))))
 
 ;;
 ;; Add on
 ;;
 
+(defvar camelsnake-python-judge-last-line nil
+  "Cache for `camelsnake-python-judge'.")
+
+(defvar camelsnake-python-judge-result nil
+  "Cache for `camelsnake-python-judge'.")
+
 (defun camelsnake-python-judge ()
   "Judge function for camelsnake in Python."
-  (let ((point (point))
-        (result (not (search-backward "class" (line-beginning-position) t))))
-    (goto-char point)
-    result))
+  (if (eq (what-line) camelsnake-python-judge-last-line)
+      camelsnake-python-judge-result
+    (let ((point (point))
+          (result (not (search-backward "class" (line-beginning-position) t))))
+      (goto-char point)
+      (setq camelsnake-python-judge-last-line (what-line))
+      (setq camelsnake-python-judge-result result))))
 
 
 (provide 'camelsnake-mode)
